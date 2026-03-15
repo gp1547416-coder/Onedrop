f43ee54c3f0c12421d4ac39d3fd647225cf36bbe
gp1547416-coder <gp1547416@gmail.com>
2026-03-15 16:18:47 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import "./globals.css"; // This MUST be here
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
