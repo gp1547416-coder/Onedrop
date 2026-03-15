c04fc36179eb5c8df952e415591a71ede41918ff
gp1547416-coder <gp1547416@gmail.com>
2026-03-15 16:12:49 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,12 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        // Adding that specific Apple/iCloud blue
-        'icloud-blue': '#007aff',
+        'ios-blue': '#007aff',
       },
-      borderRadius: {
-        'apple': '2rem',
-      }
     },
   },
   plugins: [],
